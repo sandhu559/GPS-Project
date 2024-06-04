1669aa8011c17156f996ea6e7b6295ea75334ae1
gemperador <gabriemperador@gmail.com>
2024-06-03 19:47:06 -0700

    ran into vulnerabilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,4 @@
-/*
-document.addEventListener("DOMContentLoaded", function() {
-  var map = L.map('map').setView([51.505, -0.09], 13);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-  }).addTo(map);
-
-  L.marker([51.505, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-});
-
-
-import React, { useEffect } from 'react';
+/*import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -21,13 +7,13 @@ const Map = () => {
         // Initialize the map
         const map = L.map('map').setView([51.505, -0.09], 13);
 
-        // Add a tile layer
+        // Set up the OSM layer
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     }, []);
 
-    return <div id="map"></div>;
+    return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
 };
 
 const App = () => {
@@ -42,4 +28,32 @@ const App = () => {
 export default App;
 */
 
-var marker = L.marker([38.4088, -121.3716]).addTo(map);
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+    useEffect(() => {
+        // Initialize the map
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        // Set up the OSM layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }, []);
+
+    return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
+};
+
+const App = () => {
+    return (
+        <div>
+            <h1>NavFleet Systems</h1>
+            <Map />
+        </div>
+    );
+};
+
+export default Map;
+
